d5471161b78ea1d988d67b708d69ea1a565cf75a
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-11 19:08:32 -0600

    Adding fonts, colors to tailwind.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily:{
+        consola: ['consola', 'sans-serif']
+      },
+      colors: {
+        primaryColor: '#FFF8F0',
+        secondaryColor: '#FF9000',
+        colorBackground: '#282828',
+        colorCursor: 'rgba(0,71,255,.4)',
+        colorHover: 'rgba(217,217,217,.1)',
+      }
+    },
   },
   plugins: [],
 };
