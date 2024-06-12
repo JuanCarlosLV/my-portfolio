9d36c6fc9a64637d10e26746c6276958bf42b18f
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-11 19:09:15 -0600

    Adding the follow cursor effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-
+import FollowCursor from "./Components/FollowCursor";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
 
 function App() {
   return (
     <>
-      <p>Hola</p>
+      <FollowCursor />
+
+      <header className="fixed top-0 flex justify-center items-center mt-4 w-full  z-10">
+        <Navbar />
+      </header>
+
+      <main className="px-5">
+        <section></section>
+      </main>
     </>
   );
 }
