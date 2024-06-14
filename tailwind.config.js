7671c4add91a4959375f81143fee961bbada0ea3
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-14 16:51:51 -0600

    Adding new rules of colors and typography to tailwindcss config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,16 +3,34 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      fontFamily:{
-        consola: ['consola', 'sans-serif']
+      fontFamily: {
+        corbel: ["corbel", "sans-serif"],
       },
       colors: {
-        primaryColor: '#FFF8F0',
-        secondaryColor: '#FF9000',
-        colorBackground: '#282828',
-        colorCursor: 'rgba(0,71,255,.4)',
-        colorHover: 'rgba(217,217,217,.1)',
-      }
+        primaryColor: "#FFF8F0",
+        secondaryColor: "#FF9000",
+        colorBackground: "#282828",
+        colorCursor: "rgba(0,71,255,.4)",
+        colorHover: "rgba(217,217,217,.1)",
+        colorHoverProject: "rgba(255, 144, 0, 0.1)",
+        textoSecundario: "rgba(255, 248, 240, 0.5)",
+        hoverButton: "#484848",
+        colorbgPill: "#3B3B3B",
+      },
+      keyframes: {
+        "adjust-header": {
+          "0%": { background: "transparent", height: "64px", fontSize: "16px" },
+          "100%": {
+            background: "rgba(0,0,0,0.3)",
+            backdropFilter: "blur(5px)",
+            height: "24px",
+            fontSize: "12px",
+          },
+        },
+      },
+      animation: {
+        "adjust-header": "adjust-header 1s linear both",
+      },
     },
   },
   plugins: [],
