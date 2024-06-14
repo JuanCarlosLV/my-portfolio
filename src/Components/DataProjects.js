2f7c618355fdde94a9be89157cc4a55bae35e941
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-14 16:54:11 -0600

    Adding data for projects and experiences sections

---
+++ src/Components/DataProjects.js
@@ -0,0 +1,118 @@
+const proyectos = [
+  {
+    title: "Intradeshop",
+    websiteurl: "",
+    github: "https://github.com/JuanCarlosLV/Intradeshop_web",
+    image: "/src/assets/Images/intradeshop_ss.png",
+    description:
+      "Sitio web e-commerce cuyo objetivo es brindar una plataforma a los negocios locales y brindarles una visibilidadmayor de sus productos. Creado con React, TailwindCSS y supabase",
+    tecnologies: [
+      {
+        name: "React",
+        icon: "/src/assets/Icons/react.svg",
+        colorbackground: "rgba(0,216,255,0.5)",
+      },
+      {
+        name: "Supabase",
+        icon: "/src/assets/Icons/supabase.svg",
+        colorbackground: "rgba(62,207,142,0.5)",
+      },
+      {
+        name: "TailwindCSS",
+        icon: "/src/assets/Icons/tailwindcss.svg",
+        colorbackground: "rgba(34,153,190,0.5)",
+      },
+    ],
+  },
+  {
+    title: "Sistema RECOM",
+    websiteurl: "",
+    github: "https://github.com/KeilaCruz/Sistema_Recom",
+    image: "/src/assets/Images/recom_ss.png",
+    description:  "Sitio web e-commerce cuyo objetivo es brindar una plataforma a los negocios locales y brindarles una visibilidadmayor de sus productos. Creado con React, TailwindCSS y supabase",
+    tecnologies: [
+      {
+        name: "React",
+        icon: "/src/assets/Icons/react.svg",
+        colorbackground: "rgba(0,216,255,0.5)",
+      },
+      {
+        name: "Supabase",
+        icon: "/src/assets/Icons/supabase.svg",
+        colorbackground: "rgba(62,207,142,0.5)",
+      },
+      {
+        name: "TailwindCSS",
+        icon: "/src/assets/Icons/tailwindcss.svg",
+        colorbackground: "rgba(34,153,190,0.5)",
+      },
+    ],
+  },
+];
+
+const trabajos = [
+  {
+    job: "Desarrollador Front-end",
+    place: "Universidad Veracruzana",
+    dateStarted: "Agosto 2022",
+    dateFinished: "Actualmente",
+    listWorks: [
+      "Desarrollo de páginas web para proyectos de la Universidad Veracruzana",
+      "Realización del diseño UI/UX de dichos sitios ",
+      "Análisis de requerimientos y diseño del software (sitios web)",
+    ],
+    logoEnterprise: "/src/assets/Icons/uv.svg",
+  },
+   {
+    job: "Desarrollador Front-end",
+    place: "Universidad Veracruzana",
+    dateStarted: "Agosto 2022",
+    dateFinished: "Actualmente",
+    listWorks: [
+      "Desarrollo de páginas web para proyectos de la Universidad Veracruzana",
+      "Realización del diseño UI/UX de dichos sitios ",
+      "Análisis de requerimientos y diseño del software (sitios web)",
+    ],
+    logoEnterprise: "/src/assets/Icons/uv.svg",
+  },
+];
+const skills = [
+  {
+    name: "React",
+    icon: "/src/assets/Icons/react.svg",
+  },
+  {
+    name: "Javascript",
+    icon: "/src/assets/Icons/javascript.svg",
+  },
+  {
+    name: "HTML",
+    icon: "/src/assets/Icons/html5.svg",
+  },
+  {
+    name: "CSS",
+    icon: "/src/assets/Icons/css.svg",
+  },
+  {
+    name: "Figma",
+    icon: "/src/assets/Icons/figma.svg",
+  },
+  {
+    name: "Supabase",
+    icon: "/src/assets/Icons/supabase.svg",
+  },
+  {
+    name: "Mysql",
+    icon: "/src/assets/Icons/mysql.svg",
+  },
+  {
+    name: "Git",
+    icon: "/src/assets/Icons/git.svg",
+  },
+  {
+    name: "TailwindCSS",
+    icon: "/src/assets/Icons/tailwindcss.svg",
+  },
+];
+
+export { proyectos, trabajos, skills };
