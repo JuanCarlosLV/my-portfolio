bb4c4e3c3f3d2f26fb5bc1ecede5c7f6f67d2e6f
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-22 23:11:15 -0600

    Add drop shadow style

--- src/Components/SkillPill.jsx
+++ src/Components/SkillPill.jsx
@@ -1,12 +1,17 @@
 import React from "react";
 
 function SkillPill({ skill }) {
-  
   return (
     <>
-      <article className="flex flex-col items-center justify-center w-[120px] h-[120px] p-4 gap-4 bg-colorbgPill rounded 
-      ">
-        <img src={skill.icon} alt={`Icono de ${skill.name}`} className="w-[50px]" />
+      <article
+        className="flex flex-col items-center justify-center w-[120px] h-[120px] p-4 gap-4 rounded hover-drop-shadow transition duration-300"
+        style={{ "--shadow-color": skill.color }}
+      >
+        <img
+          src={skill.icon}
+          alt={`Icono de ${skill.name}`}
+          className="w-[50px]"
+        />
         <span className="text-primaryColor text-[15px] font-semibold">
           {skill.name}
         </span>
