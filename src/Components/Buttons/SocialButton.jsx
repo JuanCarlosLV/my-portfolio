cb7e8796e47a2eb026f87655141a19c8850bc967
Juan Carlos Lopez Villegas <lopesjuancarlos216@gmail.com>
2024-06-22 23:11:54 -0600

    Check if URL exists and add effect style

--- src/Components/Buttons/SocialButton.jsx
+++ src/Components/Buttons/SocialButton.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function SocialButton({ url, logo, label }) {
+
+  const [urlAvailable, setUrlAvailable] = useState(true)
+
+  
+  useEffect(() => {
+    if (url === "") {
+      setUrlAvailable(!urlAvailable);
+    } 
+  }, [url]); 
+
   return (
     <>
       <a
-        href={url}
-        className="flex flex-row justify-center items-center  px-4 py-1 gap-2 border border-primaryColor rounded-[5px] min-w-max h-[40px]
-        hover:bg-hoverButton font-corbel "
+        href={urlAvailable ? url : "#"}
+        className={`flex flex-row justify-center items-center  px-4 py-1 gap-2 border border-primaryColor rounded-[5px] min-w-max h-[40px]
+        hover:bg-hoverButton font-corbel ${!urlAvailable && "cursor-not-allowed opacity-50 "}`}
         target="_blank"
+        rel="noreferrer noopener"
+        title={urlAvailable ? `Sitio web de ${label}` : "Sitio web no disponible"}
       >
         <img
           src={logo}
           alt={`icono de ${label}`}
           className="size-5"
+
         />
         <p className="text-primaryColor text-[14px] font-bold ">
           {label}
